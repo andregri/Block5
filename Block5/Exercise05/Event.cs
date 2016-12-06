01a249ad4198782ea26fbab4ef37e519c146483a
michisalvo <jimmyginooo@libero.it>
2016-12-06 19:36:48 +0100

    Add Event class

---
+++ Block5/Exercise05/Event.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    class Event : IEnumerable<Participant>
+    {
+        List<Participant> guests;
+        Participant guest;
+
+        public Event()
+        {
+            guests = new List<Participant>();
+        }
+
+        public void AddGuest()
+        {
+            guests.Add(guest);
+        }
+        public IEnumerator<Participant> GetEnumerator()
+        {
+            foreach (Participant guest in guests)
+            {
+                yield return guest;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
