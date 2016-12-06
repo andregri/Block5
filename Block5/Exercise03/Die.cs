aa0d5dcea8cb24380685868543492e4fcdca82a4
andregri <andrea.grillo96@live.com>
2016-12-06 22:11:16 +0100

    add Die.cs class of es2

---
+++ Block5/Exercise03/Die.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02
+{
+    public class Die : IComparable<Die>
+    {
+        private int number;
+        static private Random randomNumberSupplier = new Random((int)DateTime.Now.Ticks);
+        private const int maxNumberOfEyes = 6;
+
+        public Die()
+        {
+            number = NewToss();
+        }
+
+        public void Toss()
+        {
+            number = NewToss();
+        }
+
+        private int NewToss()
+        {
+            return randomNumberSupplier.Next(1, maxNumberOfEyes + 1);
+        }
+
+        public int Number()
+        {
+            return number;
+        }
+
+        public override String ToString()
+        {
+            return String.Format("[{0}]", number);
+        }
+
+        public int CompareTo(Die other)
+        {
+            return this.Number().CompareTo(other.Number());
+        }
+    }
+
+}
