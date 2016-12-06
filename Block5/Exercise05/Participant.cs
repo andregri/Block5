915c12e712018cbd4829c35381635554e62f2122
michisalvo <jimmyginooo@libero.it>
2016-12-06 18:24:35 +0100

    Add class Participant

---
+++ Block5/Exercise05/Participant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise05
+{
+    public class Participant
+    {
+        private int age;
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 0)
+                    age = value;
+                else
+                    throw new ArgumentOutOfRangeException("Age must be positive number");
+            }
+        }
+
+        public Participant(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
