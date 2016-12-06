4d0a31ea92b298a42362acf062e7b5f5f2f1eb8b
michisalvo <jimmyginooo@libero.it>
2016-12-06 12:52:55 +0100

    Add whole exercise, no test yet

---
+++ Block5/Exercise01/House.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise01
+{
+    public class House : PrivateProperty
+    {
+
+        protected double area;
+
+        public House(string location, bool inCity, double area,
+                     decimal value) :
+            base(location, inCity, value)
+        {
+            this.area = area;
+        }
+
+        public double Area
+        {
+            get
+            {
+                return area;
+            }
+        }
+    }
+}
---
+++ Block5/Exercise01/TaxHouse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    public class TaxHouse : House, ITaxable
+    {
+        public TaxHouse(string location, bool inCity, double area,
+             decimal value) :
+            base(location, inCity, area, value)
+        { }
+
+
+        public decimal TaxValue()
+        {
+            switch (location)
+            {
+                case "center":
+                    return (decimal)(area * 2.330);
+                case "hinterland":
+                    return (decimal)(area * 1.707);
+                case "ghetto":
+                    return (decimal)(area * 1.135);
+                case "luxury":
+                    return (decimal)(area * 3.802);
+                default:
+                    return (decimal)(area * 1.500);
+            }
+        }
+    }
+}
