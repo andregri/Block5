21ebc4557f47363fed3ef0af9c0898cb7772d0c5
andregri <andrea.grillo96@live.com>
2016-12-06 22:06:07 +0100

    test compareTo with sort()

--- Block5/ClassTest/UnitTest02.cs
+++ Block5/ClassTest/UnitTest02.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise02;
 
 namespace ClassTest
 {
     [TestClass]
     public class UnitTest02
     {
-        [TestMethod]
-        public void TestMethod1()
+        private bool IsRandom(Die[] dice)
         {
+            for (int i = 1; i < dice.Length; i++)
+            {
+                if (dice[i - 1].CompareTo(dice[i]) > 0)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        [TestMethod]
+        public void TestIComparable()
+        {
+            Die[] dice = new Die[]
+            {
+                new Die(), new Die(), new Die(), new Die(),
+                new Die(), new Die(), new Die(), new Die(),
+                new Die(), new Die(), new Die(), new Die(),
+                new Die(), new Die(), new Die(), new Die()
+            };
+
+            Assert.IsTrue(IsRandom(dice));
+            Array.Sort(dice);
+            Assert.IsFalse(IsRandom(dice));
         }
     }
 }
