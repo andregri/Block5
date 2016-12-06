8a0df4a045b5e54c85af6d7c8fb2e442900f8871
michisalvo <jimmyginooo@libero.it>
2016-12-06 12:58:03 +0100

    Add unit test

--- Block5/ClassTest/UnitTest01.cs
+++ Block5/ClassTest/UnitTest01.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise01;
 
 namespace ClassTest
 {
     [TestClass]
-    public class UnitTest01
+    public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestITaxableItems()
         {
+            decimal totalAmount = 0;
+            List<ITaxable> taxable = new List<ITaxable>();
+
+            TaxHouse centerHouse = new TaxHouse("center", true, 200, 150m);
+            TaxHouse luxuryHouse = new TaxHouse("luxury", false, 350, 500m);
+            TaxBus schoolBus = new TaxBus(25, 46, 100m);
+            TaxBus oneBus = new TaxBus(50, 1, 200m);
+
+            taxable.Add(centerHouse);
+            taxable.Add(luxuryHouse);
+            taxable.Add(schoolBus);
+            taxable.Add(oneBus);
+
+            foreach (ITaxable item in taxable)
+            {
+                totalAmount += item.TaxValue();
+            }
+
+            Assert.AreEqual(1796812.5m, totalAmount);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+
         }
     }
 }
