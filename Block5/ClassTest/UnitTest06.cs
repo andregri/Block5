8896e5ee0c03d1588953cdfe1e6b07b6da1d2207
andregri <andrea.grillo96@live.com>
2016-12-07 22:00:17 +0100

    test if nested loops are possible

--- Block5/ClassTest/UnitTest06.cs
+++ Block5/ClassTest/UnitTest06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
 using Exercise06;
 
 namespace ClassTest
@@ -14,6 +15,29 @@ namespace ClassTest
             int[] actual = (int[])i;
             int[] expected = { 10, 11, 12, 13, 14, 15 };
             CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestNestedLoop()
+        {
+            IEnumerator outerInterval = new Interval(1, 3).GetEnumerator();
+            IEnumerator innerInterval = new Interval(6, 8).GetEnumerator();
+
+            int[] result = new int[9];
+            int[] expected = {7, 8, 9, 8, 9, 10, 9, 10, 11 };
+
+            int counter = 0;
+            while (outerInterval.MoveNext())
+            {
+                while (innerInterval.MoveNext())
+                {
+                    result[counter] = (int)innerInterval.Current + (int)outerInterval.Current;
+                    counter++;
+                }
+                innerInterval.Reset();
+            }
+
+            CollectionAssert.AreEqual(expected, result);
         }
     }
 }
