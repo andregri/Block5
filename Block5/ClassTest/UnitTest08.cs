ee4cc039d5c4325e66c0d442450bf79fcbed4ba6
andregri <andrea.grillo96@live.com>
2016-12-10 13:10:04 +0100

    implement test

--- Block5/ClassTest/UnitTest08.cs
+++ Block5/ClassTest/UnitTest08.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise08;
 
 namespace ClassTest
 {
@@ -7,8 +8,25 @@ namespace ClassTest
     public class UnitTest08
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestObserverPattern()
         {
+            ObservableQuote publisher = new ObservableQuote();
+            QuoteRepository quotes = new QuoteRepository();
+            IBMObserver ibmObserver = new IBMObserver();
+            DELLObserver dellObserver = new DELLObserver();
+
+            ibmObserver.Subscribe(publisher);
+            dellObserver.Subscribe(publisher);
+
+            publisher.OpenStockExchange(quotes.GetAllQuotes());
+            Assert.AreEqual(3.2m, ibmObserver.MaxValueMln);
+            Assert.AreEqual(1.46d, ibmObserver.MaxGrowth, 0.01d);
+            Assert.AreEqual(2.9m, dellObserver.MaxValueMln);
+            Assert.AreEqual(0.38d, dellObserver.MaxGrowth, 0.01d);
+
+            publisher.CloseStockExchange();
+            ibmObserver.Unsubscribe();
+            dellObserver.Unsubscribe();
         }
     }
 }
