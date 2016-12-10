623faf2fdd8c7e890653e96781455f0b2a203a4b
andregri <andrea.grillo96@live.com>
2016-12-10 14:53:51 +0100

    create room and outside with door classes

---
+++ Block5/Exercise10_1/RoomWithDoor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10_1
+{
+    class RoomWithDoor : Room, IHasExteriorDoor
+    {
+        public RoomWithDoor(string name, string decoration, string doorDescription) : base(name, decoration)
+        {
+            DoorDescription = doorDescription;
+        }
+
+        public string DoorDescription
+        {
+            get;
+            set;
+        }
+
+        public Location DoorLocation
+        {
+            get;
+            set;
+        }
+    }
+}
