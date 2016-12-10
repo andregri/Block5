aeebdd9707580d01ab0837e6aeacda54f99b86d9
andregri <andrea.grillo96@live.com>
2016-12-10 11:15:15 +0100

    format code

--- Block5/Exercise08/QuoteRepository.cs
+++ Block5/Exercise08/QuoteRepository.cs
@@ -10,17 +10,16 @@ namespace Exercise08
     {
         public IEnumerable<Quote> GetAllQuotes()
         {
-            return new List<Quote>()
-        {
-            new Quote(){Symbol = "IBM", Price= 1}
-            , new Quote() {Symbol = "IBM", Price = 1.2m}
-            , new Quote() {Symbol = "IBM", Price = 1.3m}
-            , new Quote() {Symbol = "DELL", Price = 2.1m}
-            , new Quote() {Symbol = "IBM", Price = 3.2m}
-            , new Quote() {Symbol = "DELL", Price = 2.9m}
-            , new Quote() {Symbol = "IBM", Price = 1.8m}
-            , new Quote() {Symbol = "DELL", Price = 1.7m}
-        };
+            return new List<Quote>() {
+                new Quote(){Symbol = "IBM", Price= 1},
+                new Quote() {Symbol = "IBM", Price = 1.2m},
+                new Quote() {Symbol = "IBM", Price = 1.3m},
+                new Quote() {Symbol = "DELL", Price = 2.1m},
+                new Quote() {Symbol = "IBM", Price = 3.2m},
+                new Quote() {Symbol = "DELL", Price = 2.9m},
+                new Quote() {Symbol = "IBM", Price = 1.8m},
+                new Quote() {Symbol = "DELL", Price = 1.7m}
+            };
         }
     }
 }
