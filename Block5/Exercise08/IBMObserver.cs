891e8d5770776a06a1703071f2e2cbcf2bfcc5a5
andregri <andrea.grillo96@live.com>
2016-12-10 12:44:11 +0100

    implement observers

--- Block5/Exercise08/IBMObserver.cs
+++ Block5/Exercise08/IBMObserver.cs
@@ -6,7 +6,69 @@ using System.Threading.Tasks;
 
 namespace Exercise08
 {
-    class IBMObserver
+    class IBMObserver : IStockExchange, IObserver<Quote>
     {
+        private const string id = "IBM";
+        private IDisposable cancellation;
+        private List<double> growthHistory = new List<double>();
+        private List<decimal> valueMlnHistory = new List<decimal>();
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public double MaxGrowth
+        {
+            get
+            {
+                return growthHistory.Max();
+            }
+        }
+
+        public decimal MaxValueMln
+        {
+            get
+            {
+                return valueMlnHistory.Max();
+            }
+        }
+
+        public void Subscribe(ObservableQuote publisher)
+        {
+            cancellation = publisher.Subscribe(this);
+        }
+
+        public void Unsubscribe()
+        {
+            cancellation.Dispose();
+        }
+
+        public void OnCompleted()
+        {
+            valueMlnHistory.Clear();
+            growthHistory.Clear();
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine(error.Message);
+        }
+
+        public void OnNext(Quote value)
+        {
+            valueMlnHistory.Add(value.Price);
+
+            int count = valueMlnHistory.Count;
+            if (count > 1)
+            {
+                decimal previous = valueMlnHistory[count - 2];
+                decimal last = valueMlnHistory[count - 1];
+                growthHistory.Add((double)(last - previous) / (double)previous);
+            }
+        }
     }
 }
