1bb99a7a04e6f8128de080064fa63cebe0b3283b
andregri <andrea.grillo96@live.com>
2016-12-10 11:53:20 +0100

    implement ObservableQuote

--- Block5/Exercise08/ObservableQuote.cs
+++ Block5/Exercise08/ObservableQuote.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Exercise08
 {
-    public class ObservableQuote
+    public class ObservableQuote : IObservable<Quote>
     {
+        private Dictionary<string, List<IObserver<Quote>>> observers;
+
+        public ObservableQuote()
+        {
+            observers = new Dictionary<string, List<IObserver<Quote>>>();
+        }
+
+        public IDisposable Subscribe(IObserver<Quote> observer)
+        {
+            string id = ((IStockExchange)observer).Id;
+            if (!observers[id].Contains(observer))
+                observers[id].Add(observer);
+
+            return new QuoteUnsubscriber(observers[id], observer);
+        }
+
+        public void OpenStockExchange(IEnumerable<Quote> quotes)
+        {
+            foreach (var quote in quotes)
+            {
+                string id = quote.Symbol;
+                foreach (var observer in observers[id])
+                {
+                    observer.OnNext(quote);
+                }
+            }
+        }
+
+        public void CloseStockExchange()
+        {
+            foreach (var k in observers.Keys)
+            {
+                foreach (var observer in observers[k])
+                {
+                    observer.OnCompleted();
+                }
+            }
+        }
     }
 }
