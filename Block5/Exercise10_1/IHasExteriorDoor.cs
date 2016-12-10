ee8ba9394032bb57013cf618cb3103eb25626499
andregri <andrea.grillo96@live.com>
2016-12-10 14:44:44 +0100

    create IHasExteriorDoor interface

---
+++ Block5/Exercise10_1/IHasExteriorDoor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise10_1
+{
+    interface IHasExteriorDoor
+    {
+        string DoorDescription
+        {
+            get;
+        }
+
+        Location DoorLocation
+        {
+            get;
+            set;
+        }
+    }
+}
