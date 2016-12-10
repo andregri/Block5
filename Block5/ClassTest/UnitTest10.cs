8cee4988dafea84a9ec48b55a325aafdb1ed905a
andregri <andrea.grillo96@live.com>
2016-12-10 15:10:48 +0100

    implement first test

--- Block5/ClassTest/UnitTest10.cs
+++ Block5/ClassTest/UnitTest10.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise10_1;
 
 namespace ClassTest
 {
@@ -7,8 +8,30 @@ namespace ClassTest
     public class UnitTest10
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestPart1()
         {
+            Room diningRoom = new Room("Dining Room", "a crystal chandelier");
+            RoomWithDoor kitchen = new RoomWithDoor("Kitchen", "stainless steel appliances", "screen door");
+            RoomWithDoor livingRoom = new RoomWithDoor("Living Room", "an antique carpet", "an oak door with a brass knob");
+            Outside garden = new Outside("Garden", false);
+            OutsideWithDoor frontYard = new OutsideWithDoor("Front Yard", false, "an oak door with a brass knob");
+            OutsideWithDoor backYard = new OutsideWithDoor("Back Yard", true, "screen door");
+
+            diningRoom.Exits = new Location[] { livingRoom, kitchen };
+
+            kitchen.Exits = new Location[] { diningRoom };
+            kitchen.DoorLocation = backYard;
+
+            livingRoom.Exits = new Location[] { diningRoom };
+            livingRoom.DoorLocation = frontYard;
+
+            garden.Exits = new Location[] { backYard, frontYard };
+
+            frontYard.Exits = new Location[] { garden };
+            frontYard.DoorLocation = livingRoom;
+
+            backYard.Exits = new Location[] { garden };
+            backYard.DoorLocation = kitchen;
         }
     }
 }
