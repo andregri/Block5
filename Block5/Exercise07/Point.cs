527398b583232665a8fdeee80f4cb85e68cea280
michisalvo <jimmyginooo@libero.it>
2016-12-14 17:32:42 +0100

    Add new class Point

---
+++ Block5/Exercise07/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise07
+{
+    public class Point
+    {
+        private double coordinateX;
+        private double coordinateY;
+
+        public double CoordinateX
+        {
+            get { return coordinateX; }
+            set { coordinateX = value; }
+        }
+
+
+        public Point(double x, double y)
+        {
+            CoordinateX = x;
+            CoordinateY = y;
+        }
+    }
+}
