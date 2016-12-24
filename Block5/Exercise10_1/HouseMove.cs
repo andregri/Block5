889cecee8e08b9481706ed02e1af76bfbccaf04a
michisalvo <jimmyginooo@libero.it>
2016-12-24 17:35:11 +0100

    Add new class HouseMove.cs

---
+++ Block5/Exercise10_1/HouseMove.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Exercise10_1
+{
+    public class HouseMove 
+    {
+        private Location CurrentLocation { get; set; }
+
+        public HouseMove(Location currentLocation) 
+        {
+            CurrentLocation = currentLocation;
+        }
+
+		void MoveToANewLocation(Location newLocation)
+		{
+			CurrentLocation = newLocation;
+		}
+
+        public Location[] Exits
+        {
+            get { return CurrentLocation.Exits; }
+        }
+
+        void GoThroughExteriorDoor()
+		{
+            // create an instance of something that implements the interface allow us to 
+            // distinguish between class "[...]WithDoor" and not 
+            IHasExteriorDoor hasExitDoor = CurrentLocation as IHasExteriorDoor;
+			if (hasExitDoor != null)
+				MoveToANewLocation(CurrentLocation);
+			else
+				throw new ArgumentNullException("Sorry this place hasn't an Exterior Door");
+		}
+    }
+}
